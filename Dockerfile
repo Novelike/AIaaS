20b718dc4b34e1fc66a0875eee2ea270df606eaa
kjh <kimjeahyun77@gmail.com>
2025-03-25 16:08:20 +0900

    Favicon Error Fix
    Add favicon.png, modified main.py

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 FROM python:3.9-slim
 
 # 작업 디렉토리 설정
-WORKDIR /testing
+WORKDIR /app
 
 # 의존성 파일 복사 및 설치
 # COPY requirements.txt .
@@ -11,7 +11,7 @@ RUN pip install fastapi
 RUN pip install uvicorn
 
 # 애플리케이션 코드 복사
-COPY ./app /testing
+COPY ./app /app
 
 # 컨테이너 실행 시 명령어
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 ROOT_PATH = "/"
@@ -8,8 +8,10 @@ app = FastAPI()
 # 1. 정적 파일 설정
 app.mount('/static', StaticFiles(directory='.'), name='static')
 
+@app.get("/favicon.ico")
+async def get_favicon():
+    return FileResponse("favicon.png")
 
 @app.get(ROOT_PATH)
 async def get_root_message():
     return {"message": "Hello World!!"}
-    
